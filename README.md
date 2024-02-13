89033bc0eb177856347394bb093abcf5ac3c6d3a
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-02-13 14:40:40 +0100

    more setup

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # LinkUp
 Disord clone
+
+## Run react server
+run `bun run serve` and startup a liveserver and open localhost:3000 in your browser.
---
+++ srcfront/index.js
@@ -0,0 +1,5 @@
+import { createRoot } from 'react-dom/client'
+import App from './components/App/App'
+
+const Root = createRoot(document.getElementById('root'))
+Root.render(<App />)
