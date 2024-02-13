3c0b190eec92f8e4faccba03778e2a6ca86132bd
NielsOostindie <112725677+NielsOostindie@users.noreply.github.com>
2024-02-13 15:52:53 +0100

    login

---
+++ srcfront/src/Components/Login/Login.jsx
@@ -0,0 +1,15 @@
+import './Login.css'
+
+function Login() {
+  return (
+    <>
+        <form action="">
+            <input type="text" placeholder='username' required />
+            <br />
+            <input type="submit" value="Login" />
+        </form>
+    </>
+  )
+}
+
+export default Login
