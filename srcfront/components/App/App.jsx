1753391a7ed43c7114c263fc50ad3f470e24cb1a
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-02-13 15:45:56 +0100

    yeet

--- srcfront/components/App/App.jsx
+++
@@ -1,9 +0,0 @@
-import Login from "../Login/Login.jsx"
-
-export default function App() {
-    return (
-        <div>
-            <Login />
-        </div>
-    )
-}
--- srcfront/index.js
+++
@@ -1,6 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import App from './components/App/App'
-
-
-const Root = createRoot(document.getElementById('root'))
-Root.render(<App />)
