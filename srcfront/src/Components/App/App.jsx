8d636df87f8e9ce1d258ebdc162d7b72df616519
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-02-14 13:21:20 +0100

    tiny tiny rework to login page

--- srcfront/src/Components/App/App.jsx
+++ srcfront/src/Components/App/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import Login from '../Login/Login'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Login />
