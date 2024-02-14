9fb6e62d853588630deb32b297182b0c3dc9df5e
NielsOostindie <112725677+NielsOostindie@users.noreply.github.com>
2024-02-14 13:36:27 +0100

    message beta

--- srcfront/src/Components/App/App.jsx
+++ srcfront/src/Components/App/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 // import Login from '../Login/Login'
-import Chat from '../Chat/Chat'
+// import Chat from '../Chat/Chat'
+import Message from '../Message/Message'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Chat />
+      {/* <Login/> */}
+      {/* <Chat /> */}
+      <Message />
     </>
   )
 }
