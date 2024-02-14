93819b2295018caf339c99e2e09eb499afff9007
NielsOostindie <112725677+NielsOostindie@users.noreply.github.com>
2024-02-14 12:57:44 +0100

    indent

--- srcfront/src/Components/Login/Login.jsx
+++ srcfront/src/Components/Login/Login.jsx
@@ -1,22 +1,22 @@
-import './Login.css'
+import "./Login.css";
 
 function Login() {
   return (
     <>
-<div class="container">
-  <div class="card">
-    <h2>Login</h2>
-    <form action=''>
-    <label for="username">Username</label>
-      <input type="text" id="username" name="username" required/>
-      <label for="password">Password</label>
-      <input type="password" id="password" name="password" required/>
-      <button type="submit">Login</button>
-    </form>
-  </div>
-</div>
+      <div class="container">
+        <div class="card">
+          <h2>Login</h2>
+          <form action="">
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username" required />
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" required />
+            <button type="submit">Login</button>
+          </form>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Login
+export default Login;
