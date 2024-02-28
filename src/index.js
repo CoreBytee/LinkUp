9515d56e2f4477c4fd4bb0ef307765086b6ccdb3
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-02-28 12:59:03 +0100

    ws server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-import LinkUpClass from "./Classes/LinkUp"
+// import LinkUpClass from "./Classes/LinkUp"
 
-const LinkUp = new LinkUpClass()
+// const LinkUp = new LinkUpClass()
+
+Bun.serve(
+    {
+        port: 3000,
+        fetch: (Request, Server) => {
+            console.log("Request received")
+            Server.upgrade(
+                Request
+            )
+            return
+        },
+        websocket: {
+            open: (Websocket) => {
+                Websocket.subscribe("messages")
+            },
+            message: (Websocket, Message) => {
+                console.log(Message)
+                Websocket.publish("messages", Message)
+                Websocket.send(Message)
+            },
+            close: (Websocket) => {
+                Websocket.unsubscribe("messages")
+            }
+        }
+    }
+)
